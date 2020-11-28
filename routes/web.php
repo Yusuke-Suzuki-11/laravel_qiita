034f4f50f127f8f5d5bc05fcebc9bb2f13085690
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2020-11-28 13:58:18 +0900

    ビュー作成

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/drafts/new', 'Auth\PostController@new')->name('drafts.new'); // 追記
